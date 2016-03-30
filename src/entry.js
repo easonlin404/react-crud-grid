d38697e50dae4d6afe92a6e0afa52679090fa39b
easonlin MacBook Air <easonlin404@gmail.com>
2016-03-30 22:28:47 +0800

    add data

--- src/entry.js
+++ src/entry.js
@@ -4,10 +4,26 @@ var ReactDOM = require('react-dom');
 
 var ReactCrudGrid = require('./react-crud-grid');
 
+var data =[
+{	firstName: 'Mark',
+	lastName:	 'Otto',
+	userName:	'@mdo'
+},
+{
+	firstName: 'Jacob',
+	lastName:	 'Thornton',
+	userName:	'@fat'
+},
+{
+	firstName: 'Larry the Bird',
+	userName:	'@twitter'
+}
+];
+
 var SPA = React.createClass({
 	render: function() {
     return (
-      <ReactCrudGrid/>
+      <ReactCrudGrid data={data}/>
     );
   }
 });
